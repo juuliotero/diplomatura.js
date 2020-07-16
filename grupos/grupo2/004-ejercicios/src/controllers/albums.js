b1c4e135f19ccff9f3e9dcf57ff4b822d1bfb969
jotero <jotero@jotero>
2020-07-16 20:26:56 -0300

    feat: inciso 3

--- grupos/grupo2/004-ejercicios/src/controllers/albums.js
+++ grupos/grupo2/004-ejercicios/src/controllers/albums.js
@@ -1,13 +1,48 @@
 import express from 'express';
+import fetch from 'node-fetch';
 
 var albumsApi = express.Router();
 
-albumsApi.get('/', function (req, res) {
-  res.send('Hola albums!.');
+albumsApi.get('/', async function (req, res) {
+  try {
+    let responseUsers = await fetch(`https://jsonplaceholder.typicode.com/users/`);
+    let usersData = await responseUsers.json();
+    let responseAlbums = await fetch(`https://jsonplaceholder.typicode.com/albums/`);
+    let albumsData = await responseAlbums.json();
+
+    let respuestaFinal = await albumsData.map((album) => {
+      return {
+        user: usersData.find((u) => u.id === album.userId),
+        id: album.id,
+        title: album.title,
+        body: album.body,
+      };
+    });
+    res.send(respuestaFinal);
+  } catch (error) {
+    res.send(error);
+  }
 });
 
-albumsApi.get('/:id', function (req, res) {
-  res.send(`Hola album ${req.params.id}.`);
+albumsApi.get('/:id', async function (req, res) {
+  try {
+    let responseAlbums = await fetch(`https://jsonplaceholder.typicode.com/albums/${req.params.id}`);
+    let albumsData = await responseAlbums.json();
+
+    let responsePhotos = await fetch(`https://jsonplaceholder.typicode.com/albums/${req.params.id}/photos`);
+    let photosData = await responsePhotos.json();
+    photosData = await photosData.map((photo) => {
+      delete photo.albumId;
+      return {
+        photo
+      };
+    });
+    albumsData.photos = photosData;
+
+    res.send(albumsData);
+  } catch (error) {
+    res.send(error);
+  }
 });
 
 export default albumsApi;
