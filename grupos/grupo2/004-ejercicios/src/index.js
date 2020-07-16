293644e3bdbf813dfefc631205428b02828d3152
Pancho <martinebucarey@gmail,com>
2020-07-16 20:10:21 -0300

    feat: agrega endpoint de stats

--- grupos/grupo2/004-ejercicios/src/index.js
+++ grupos/grupo2/004-ejercicios/src/index.js
@@ -2,6 +2,9 @@ import express from 'express';
 const app = express();
 import postsRoutes from './controllers/posts';
 import albumRoutes from './controllers/albums';
+import os from 'os';
+import moment from 'moment';
+moment.locale('es');
 
 const PORT = 8080;
 
@@ -10,7 +13,20 @@ app.use('/albums', albumRoutes);
 
 // Implementar el endpoint de stats aca. GET "/"
 app.get('/', function (req, res) {
-  res.json({ mensaje: 'Hello world!' });
+  const result = {
+    serverCurrentTime: moment(new Date()).format('MMMM Do YYYY, h:mm:ss a'), // En español
+    serverStartUpTime: moment(inicio).format('MMMM Do YYYY, h:mm:ss a'), // En español
+    serverUpTime: moment(inicio).startOf('minutes').fromNow(), // usando moment relative time
+
+    status: {
+      freemem: os.freemem(),
+      totalmem: os.totalmem(),
+      uptime: os.uptime(),
+      hostname: os.hostname(),
+      platform: os.platform(),
+    },
+  };
+  res.json({ result });
 });
 
 app.listen(PORT);
