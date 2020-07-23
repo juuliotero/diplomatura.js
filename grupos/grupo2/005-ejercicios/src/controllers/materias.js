fd1f2cc98f2b5727672e9078c3be2fa299694a55
carito-ro <kro.robles@gmail.com>
2020-07-23 18:57:32 -0300

    Ejercicio materias

--- grupos/grupo2/005-ejercicios/src/controllers/materias.js
+++ grupos/grupo2/005-ejercicios/src/controllers/materias.js
@@ -1,8 +1,10 @@
 import express from 'express';
+//import mongoose from 'mongoose';
 import { helpers } from '../helpers';
 const router = express.Router();
 
 router.get('/', async function (req, res) {
+    console.log("getAll");
     helpers.getAll('materias', {}, req, res);
 });
 
@@ -14,6 +16,16 @@ router.get('/:nombre', async function (req, res) {
 router.delete('/:id', async function (req, res) {
     const id = parseInt(req.params.id);
     helpers.delete('materias', { id: id }, req, res);
+});
+router.post('/', async function (req, res) {
+    const { id, nombre, universidad, profesores } = req.body;
+    helpers.insert('materias', { id, nombre, universidad, profesores }, req, res);
+});
+
+router.put('/:id', async function (req, res) {
+    const id = parseInt(req.params.id);
+    const datos = req.body;
+    helpers.update('materias', { id: id }, datos, req, res);
 });
 
 export default router;
--- grupos/grupo2/005-ejercicios/src/helpers.js
+++ grupos/grupo2/005-ejercicios/src/helpers.js
@@ -28,7 +28,8 @@ export const helpers = {
         try {
             const db = await connection();
             var collection = db.collection(table);
-            const resultado = await collection.findOneAndUpdate(query, { $set: data });
+            const resultado = await collection.findOneAndUpdate(query, { $set: data }, { returnOriginal: false });
+            //const resultado = await collection.findOneAndUpdate(query, { $set: data });
             if (resultado.value) {
                 res.json(resultado.value);
             } else {
